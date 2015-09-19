c0e66e9ecee2c6c002cc14617d6eed1e73ef9ffd
Samer Buna <samer.buna@gmail.com>
2015-09-18 17:05:39 -0700

    For Mike only

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+import Twitter from 'twitter';
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let { accessTokenKey, accessTokenSecret, tweetText } = req.query;
+
+  var client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: accessTokenKey,
+    access_token_secret: accessTokenSecret
+  });
+
+  let { callback } = req.query;
+
+  client.post("statuses/update", { status: tweetText }, function(err, tweet) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Something wrong happened");
+      return;
+    }
+    res.send(`${callback}({ "response": "ok" })`);
+  });
+
 });
 
 module.exports = router;
